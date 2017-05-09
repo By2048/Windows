91e48316690a3a4c5e5f26dcfc985a04641c458e
By2048 <1247079575@qq.com>
2017-05-09 18:57:07 +0800

    ListView Test 排序

--- PictureViewer/WinFormTest/Form11-ListView.cs
+++ PictureViewer/WinFormTest/Form11-ListView.cs
@@ -17,11 +17,17 @@ namespace WinFormTest
         {
             InitializeComponent();
         }
+        private void Form11_Load(object sender, EventArgs e)
+        {
+            CenterToScreen();
+            CreateMyListView();
+        }
+
         private ListView listView1;
         private void CreateMyListView()
         {
             listView1 = new ListView();
-            listView1.Bounds = new Rectangle(new Point(10, 10), new Size(300, 200));
+            listView1.Bounds = new Rectangle(new Point(10, 10), new Size(500, 400));
 
             // Set the view to show details.
             listView1.View = View.Details;
@@ -36,8 +42,16 @@ namespace WinFormTest
             // Display grid lines.
             listView1.GridLines = true;
             // Sort the items in the list in ascending order.
-            listView1.Sorting = SortOrder.Ascending;
+            //listView1.Sorting = SortOrder.Ascending;
 
+
+            // Width of -2 indicates auto-size.
+            listView1.Columns.Add("Column 1", 100, HorizontalAlignment.Left);
+            listView1.Columns.Add("Column 2", 100, HorizontalAlignment.Center);
+            listView1.Columns.Add("Column 3", -2, HorizontalAlignment.Left);
+            listView1.Columns.Add("Column 4", -2, HorizontalAlignment.Center);
+
+            listView1.Items.Clear();
             // Create three items and three sets of subitems for each item.
             ListViewItem item1 = new ListViewItem("item1", 0);
             item1.Checked = true;
@@ -59,13 +73,8 @@ namespace WinFormTest
             item4.SubItems.Add("11");
             item4.SubItems.Add("12");
 
-            // Width of -2 indicates auto-size.
-            listView1.Columns.Add("Item Column", 100, HorizontalAlignment.Left);
-            listView1.Columns.Add("Column 2", -2, HorizontalAlignment.Left);
-            listView1.Columns.Add("Column 3", -2, HorizontalAlignment.Left);
-            listView1.Columns.Add("Column 4", -2, HorizontalAlignment.Center);
+            listView1.Items.AddRange(new ListViewItem[] { item1, item4, item3, item2 });
 
-            listView1.Items.AddRange(new ListViewItem[] { item1, item4, item3 ,item2});
 
             // Create two ImageList objects.
             ImageList imageListSmall = new ImageList();
@@ -83,46 +92,179 @@ namespace WinFormTest
 
             listView1.ColumnClick += new ColumnClickEventHandler(ListView_ColumnClick);
 
+            listView1.ItemChecked += new ItemCheckedEventHandler(listView1_ItemChecked);
+
+            // listview根据列名自动调整列宽
+            listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+
+            listView1.ListViewItemSorter = listViewItemSort;
+
             this.Controls.Add(listView1);
         }
 
+        ListViewItemSort listViewItemSort=  new ListViewItemSort();
         private void ListView_ColumnClick(object sender, ColumnClickEventArgs e)
         {
-            MessageBox.Show(e.Column.ToString());
-            listView1.ListViewItemSorter = new ListViewItemComparer(e.Column);
-            //listView1.Sort();
+            if (e.Column == listViewItemSort.SortColumnValue)
+            {
+                // 重新设置此列的排序方法.
+                if (listViewItemSort.OrderValue == SortOrder.Ascending)
+                {
+                    listViewItemSort.OrderValue = SortOrder.Descending;
+                }
+                else
+                {
+                    listViewItemSort.OrderValue = SortOrder.Ascending;
+                }
+            }
+            else
+            {
+                // 设置排序列，默认为正向排序
+                listViewItemSort.SortColumnValue = e.Column;
+                listViewItemSort.OrderValue = SortOrder.Ascending;
+            }
+            // 用新的排序方法对ListView排序
+            this.listView1.Sort();
         }
 
-        private void Form11_Load(object sender, EventArgs e)
+        // checkbox的checked为true行变红，false时黑色
+        private void listView1_ItemChecked(object sender, ItemCheckedEventArgs e)
         {
-            CreateMyListView();
+
+            ListViewItem MyItem = listView1.Items[e.Item.Index];
+
+            foreach (ListViewItem item in this.listView1.Items)
+            {
+                if (MyItem.Checked)
+                {
+                    MyItem.ForeColor = Color.Red;
+                }
+                else
+                {
+                    MyItem.ForeColor = Color.Black;
+                }
+            }
         }
+
+
+        //单击行读取行数据：
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                MessageBox.Show(this.listView1.SelectedItems[0].SubItems[2].Text);
+            }
+        }
+
+        //遍历删除listview中checkbox被选中的记录
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in this.listView1.CheckedItems)
+            {
+                MessageBox.Show(item.Index.ToString());
+                this.listView1.Items.RemoveAt(item.Index);
+            }
+        }
+
+
+        // 遍历listview，把上面的数据存到list<T>中
+        private void button3_Click(object sender, EventArgs e)
+        {
+            List<TestInfo> m_CustomerServList = new List<TestInfo>();
+            foreach (ListViewItem item in listView1.Items)
+            {
+                string A, B, C, D;
+                A = item.SubItems[0].Text;
+                B = item.SubItems[1].Text;
+                C = item.SubItems[2].Text;
+                D = item.SubItems[2].Text;
+                m_CustomerServList.Add(new TestInfo(A, B, C, D));
+                //m_CustomerServList.Add(new TestInfo()
+                //{ A = item.SubItems[0].Text,
+                //    B = item.SubItems[1].Text,
+                //    C = item.SubItems[2].Text,
+                //    D = item.SubItems[2].Text,
+                //});
+                MessageBox.Show(item.SubItems[0].Text + "\n" +
+                    item.SubItems[1].Text + "\n" +
+                    item.SubItems[2].Text + "\n" +
+                    item.SubItems[3].Text);
+            }
+        }
+
     }
-    
+
 
 
 }
 
-
-public class ListViewItemComparer : IComparer
+public class TestInfo
 {
-    private int column;
-
-    public ListViewItemComparer(int column)
+    public string A;
+    public string B;
+    public string C;
+    public string D;
+    public TestInfo(string a, string b, string c, string d)
     {
-        this.column = column;
+        A = a;
+        B = b;
+        C = c;
+        D = d;
+    }
+}
+
+
+public class ListViewItemSort : IComparer
+{
+    private int SortColumn;
+    private SortOrder OrderOfSort;    // 排序方式.  
+
+    public ListViewItemSort()
+    {
+        SortColumn = 0;
+        OrderOfSort = SortOrder.None;
+    }
+
+    public ListViewItemSort(int column, SortOrder orderOfSort)
+    {
+        SortColumn = column;
+        OrderOfSort = orderOfSort;
     }
 
     public int Compare(object x, object y)
     {
+        int compareResult;
         int numX, numY;
-        bool xIsNum = int.TryParse(((ListViewItem)x).SubItems[column].Text, out numX);
-        bool yIsNum = int.TryParse(((ListViewItem)x).SubItems[column].Text, out numY);
+        ListViewItem listviewX = (ListViewItem)x, listviewY = (ListViewItem)y;
+
+        bool xIsNum = int.TryParse((listviewX).SubItems[SortColumn].Text, out numX);
+        bool yIsNum = int.TryParse((listviewY).SubItems[SortColumn].Text, out numY);
+        
+        //MessageBox.Show(numX.ToString());
+        //MessageBox.Show(numY.ToString());
         if (xIsNum && yIsNum)
-            return numX - numY;
+            compareResult = numX - numY;
         else
-            return String.Compare(((ListViewItem)x).SubItems[column].Text,
-                ((ListViewItem)y).SubItems[column].Text);
+            compareResult = String.Compare((listviewX).SubItems[SortColumn].Text, (listviewY).SubItems[SortColumn].Text);
+
+        if (OrderOfSort == SortOrder.Ascending)
+            return compareResult;
+        else if (OrderOfSort == SortOrder.Descending)
+            return (-compareResult);
+        else
+            return 0;
+    }
+
+    public int SortColumnValue
+    {
+        set{SortColumn = value;}
+        get{return SortColumn;}
+    }
+ 
+    public SortOrder OrderValue
+    {
+        set{OrderOfSort = value;}
+        get{return OrderOfSort;}
     }
 
 }
