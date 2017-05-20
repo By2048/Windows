81844d5d45071337848cfbecbf7082d3af24f569
By2048 <1247079575@qq.com>
2017-05-20 19:36:32 +0800

    代码清理

--- PictureViewer/WinFormTest/Form00-Test.cs
+++ PictureViewer/WinFormTest/Form00-Test.cs
@@ -20,26 +20,8 @@ namespace WinFormTest
 
         private void Form00_Load(object sender, EventArgs e)
         {
-            UserControlTest us1 = new UserControlTest();
-            us1.delevent += new UserControlTest.deleset(set);
-            this.panel1.Controls.Add(us1);
+           
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {         
-
-        }
-        public void set(string m)
-        {
-            textBox1.Text = m;
-
-            UserControlTest2 us1 = new UserControlTest2(textBox1.Text);
-            this.panel2.Controls.Add(us1);
-        }
-
-        private void pictureBox1_DoubleClick(object sender, EventArgs e)
-        {
-
-        }
     }
 }
